c2666adeccb617be56fb9d65d292586d653f77c9
Pawel <pawel540@o2.pl>
2021-05-07 03:50:30 +0200

    add new files

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Admin;
+use App\Entity\User;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin-register", name="api_admin-register", methods={"POST"})
+     */
+    public function register(EntityManagerInterface $om, UserPasswordEncoderInterface $passwordEncoder, Request $request)
+    {
+        $admin = new Admin();
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $passwordConfirmation = $request->request->get("password_confirmation");
+
+
+        $errors = [];
+        if ($password != $passwordConfirmation) {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+        if (strlen($password) < 6) {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+        if (!$errors) {
+            $encodedPassword = $passwordEncoder->encodePassword($admin, $password);
+            $admin->setEmail($email);
+            $admin->setPassword($encodedPassword);
+            try {
+                $om->persist($admin);
+                $om->flush();
+
+                return $this->json([
+                    'admin' => $admin
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $errors[] = "The email provided already has an account!";
+            } catch (\Exception $e) {
+                $errors[] = "Unable to save new user at this time.";
+            }
+        }
+
+        return $this->json([
+            'errors' => $errors
+        ], 400);
+    }
+
+    /**
+     * @Route("/admin-login", name="api_admin-login", methods={"POST"})
+     */
+    public function login()
+    {
+        return $this->json(['result' => true]);
+    }
+
+    /**
+     * @Route("/admin-profile", name="api_admin-profile")
+     * @IsGranted("ROLE_USER")
+     */
+    public function profile()
+    {
+        return $this->json([
+            'admin' => $this->getUser()
+        ]);
+    }
+
+    /**
+     * @Route("/admin-edit{id}", name="api_users_edit", methods={"POST"})
+     */
+    public function EditUserAction($id, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->getDoctrine()->getRepository('CoreBundle:User')->findOneBy(['id' => $id, 'deleted' => 0]);
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $passwordConfirmation = $request->request->get("password_confirmation");
+
+
+        $errors = [];
+        if ($password != $passwordConfirmation) {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+        if (strlen($password) < 6) {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+        if (!$errors) {
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+            try {
+                $om->persist($user);
+                $om->flush();
+
+                return $this->json([
+                    'user' => $user
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $errors[] = "The email provided already has an account!";
+            } catch (\Exception $e) {
+                $errors[] = "Unable to save new user at this time.";
+            }
+        }
+
+        return $this->json([
+            'errors' => $errors
+        ], 400);
+    }
+
+    /**
+     * @Route("/", name="api_home")
+     */
+    public function home()
+    {
+        return $this->json(['result' => true]);
+    }
+
+
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/register", name="api_register", methods={"POST"})
+     */
+    public function register(EntityManagerInterface $om, UserPasswordEncoderInterface $passwordEncoder, Request $request)
+    {
+        $user = new User();
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $passwordConfirmation = $request->request->get("password_confirmation");
+
+
+        $errors = [];
+        if ($password != $passwordConfirmation) {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+        if (strlen($password) < 6) {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+        if (!$errors) {
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+            try {
+                $om->persist($user);
+                $om->flush();
+
+                return $this->json([
+                    'user' => $user
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $errors[] = "The email provided already has an account!";
+            } catch (\Exception $e) {
+                $errors[] = "Unable to save new user at this time.";
+            }
+        }
+
+        return $this->json([
+            'errors' => $errors
+        ], 400);
+    }
+
+    /**
+     * @Route("/login", name="api_login", methods={"POST"})
+     */
+    public function login()
+    {
+        return $this->json(['result' => true]);
+    }
+
+    /**
+     * @Route("/profile", name="api_profile")
+     * @IsGranted("ROLE_USER")
+     */
+    public function profile()
+    {
+        return $this->json([
+            'user' => $this->getUser()
+        ]);
+    }
+
+    /**
+     * @Route("/edit{id}", name="api_users_edit", methods={"POST"})
+     */
+    public function EditUserAction($id, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = $this->getDoctrine()->getRepository('CoreBundle:User')->findOneBy(['id' => $id, 'deleted' => 0]);
+        $email = $request->request->get("email");
+        $password = $request->request->get("password");
+        $passwordConfirmation = $request->request->get("password_confirmation");
+
+
+        $errors = [];
+        if ($password != $passwordConfirmation) {
+            $errors[] = "Password does not match the password confirmation.";
+        }
+        if (strlen($password) < 6) {
+            $errors[] = "Password should be at least 6 characters.";
+        }
+        if (!$errors) {
+            $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+            $user->setEmail($email);
+            $user->setPassword($encodedPassword);
+            try {
+                $om->persist($user);
+                $om->flush();
+
+                return $this->json([
+                    'user' => $user
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $errors[] = "The email provided already has an account!";
+            } catch (\Exception $e) {
+                $errors[] = "Unable to save new user at this time.";
+            }
+        }
+
+        return $this->json([
+            'errors' => $errors
+        ], 400);
+    }
+
+    /**
+     * @Route("/", name="api_home")
+     */
+    public function home()
+    {
+        return $this->json(['result' => true]);
+    }
+
+}
+
